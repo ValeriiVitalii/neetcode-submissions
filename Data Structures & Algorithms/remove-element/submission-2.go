498fa8fac379a9d928e4ba612595382219390261
Vale <vgorbat1998@yandex.ru>
2026-05-11 20:51:45 +0300

    Add: remove-element - submission-2

---
+++ Data Structures & Algorithms/remove-element/submission-2.go
@@ -0,0 +1,13 @@
+func removeElement(nums []int, val int) int {
+	writeIndex := 0
+
+	for i:=0;i<len(nums);i++{
+		if nums[i] != val {
+			nums[writeIndex] = nums[i]
+
+			writeIndex++
+		}
+	}
+
+	return writeIndex
+}
